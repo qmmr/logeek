f4490120fd45076232d9671b0a2646203af25fa3
Marcin Kumorek <kumeek@gmail.com>
2018-02-27 13:00:48 +0100

    Run prettier on files and update .eslintrc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
+import React, { Component } from 'react'
+
+import logo from './logo.svg'
+
+/* eslint-disable react/prefer-stateless-function */
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<img src={ logo } className='App-logo' alt='logo' />
+					<h1 className='App-title'>Welcome to React</h1>
+				</header>
+				<p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+				</p>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
